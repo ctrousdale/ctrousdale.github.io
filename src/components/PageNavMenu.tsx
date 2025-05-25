3d8749686b2d53686b9582af426926ddc4803e63
Chandler Trousdale <chandler2506@gmail.com>
2025-05-25 18:42:55 -0400

    Update nav menu color

--- src/components/PageNavMenu.tsx
+++ src/components/PageNavMenu.tsx
@@ -2,58 +2,54 @@ import * as React from "react";
 import {
   NavigationMenu,
   NavigationMenuItem,
+  NavigationMenuLink,
   NavigationMenuList,
 } from "./ui/navigation-menu";
-import { NavigationMenuLink } from "@radix-ui/react-navigation-menu";
 import { Separator } from "./ui/separator";
 import { NavLink } from "react-router";
 import { FaGithub } from "react-icons/fa6";
 
-const PageNavMenu = (): React.JSX.Element => {
-  return (
-    <div className="sticky w-full">
-      <div className="flex items-center ">
-        <NavigationMenu className="bg-amber-900 max-w-full">
-          <NavigationMenuList className="space-x-4 flex items-center">
-            <NavigationMenuItem>
-              <NavLink
-                to="/"
-                className={({ isActive }) => (isActive ? "text-amber-200" : "")}
-              >
-                <NavigationMenuLink className="text-xl">
-                  About me
-                </NavigationMenuLink>
-              </NavLink>
-            </NavigationMenuItem>
-            <Separator orientation="vertical" />
-            <NavigationMenuItem>
-              <NavLink
-                to="/blog"
-                className={({ isActive }) => (isActive ? "text-amber-200" : "")}
-              >
-                <NavigationMenuLink className="text-xl">
-                  Blog
-                </NavigationMenuLink>
-              </NavLink>
-            </NavigationMenuItem>
-            <Separator orientation="vertical" />
-            <NavigationMenuItem>
-              <NavLink
-                to="https://github.com/ctrousdale"
-                target="_blank"
-                rel="noopener noreferrer"
-                className={({ isActive }) => (isActive ? "text-amber-200" : "")}
-              >
-                <NavigationMenuLink>
-                  <FaGithub />
-                </NavigationMenuLink>
-              </NavLink>
-            </NavigationMenuItem>
-          </NavigationMenuList>
-        </NavigationMenu>
-      </div>
+const PageNavMenu = (): React.JSX.Element => (
+  <div className="sticky w-full top-0 z-50">
+    <div className="flex items-center ">
+      <NavigationMenu className="bg-stone-800/80 backdrop-blur-md max-w-full">
+        <NavigationMenuList className="space-x-4 flex items-center">
+          <NavigationMenuItem>
+            <NavLink
+              to="/"
+              className={({ isActive }) => (isActive ? "text-amber-200" : "")}
+            >
+              <NavigationMenuLink className="text-xl">
+                About me
+              </NavigationMenuLink>
+            </NavLink>
+          </NavigationMenuItem>
+          <Separator orientation="vertical" />
+          <NavigationMenuItem>
+            <NavLink
+              to="/blog"
+              className={({ isActive }) => (isActive ? "text-amber-200" : "")}
+            >
+              <NavigationMenuLink className="text-xl">Blog</NavigationMenuLink>
+            </NavLink>
+          </NavigationMenuItem>
+          <Separator orientation="vertical" />
+          <NavigationMenuItem>
+            <NavLink
+              to="https://github.com/ctrousdale"
+              target="_blank"
+              rel="noopener noreferrer"
+              className={({ isActive }) => (isActive ? "text-amber-200" : "")}
+            >
+              <NavigationMenuLink>
+                <FaGithub />
+              </NavigationMenuLink>
+            </NavLink>
+          </NavigationMenuItem>
+        </NavigationMenuList>
+      </NavigationMenu>
     </div>
-  );
-};
+  </div>
+);
 
 export default PageNavMenu;
